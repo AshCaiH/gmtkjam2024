bd206244402c2580f69c90cc6da2098f2920de9c
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-08-16 21:27:39 +0100

    collision detection

--- scripts/Sponge.gd
+++ scripts/Sponge.gd
@@ -1,22 +1,35 @@
-extends Node3D
+extends RigidBody3D
 
-var growSpeed = 10;
+var growSpeed = 10.0;
 var satisfied = false;
+var oldscale = scale;
+var scaleX = true;
+var scaleZ = true;
 @onready var mesh = $MeshInstance3D.mesh
 
 func _ready():  
-
-    # print($MeshInstance3D.mesh.get_aabb())
-
     for i in range(8):
         var endpoints = mesh.get_aabb().get_endpoint(i)
         print(to_global(endpoints))
 
-func _process(delta):
-    if !satisfied:
-        # var oldScale = scale.x;
+    contact_monitor = true;
+    max_contacts_reported = 2;
 
-        scale.x += growSpeed * delta;
+    scaleX = randi() % 2;
+    scaleZ = randi() % 2;
+
+    add_to_group("sponges");
+
+func _physics_process(delta: float) -> void:
+    if !satisfied:
+        oldscale = scale;
+
+        if scaleX and scaleZ:
+            scale += Vector3(growSpeed / 2, 0, growSpeed / 2) * delta;
+        elif scaleX:
+            scale.x += growSpeed  * delta;
+        elif scaleZ:
+            scale.z += growSpeed  * delta;
 
         for i in range(8):
             var endpoints = to_global(mesh.get_aabb().get_endpoint(i))
@@ -24,16 +37,10 @@ func _process(delta):
                 or endpoints.z < -1 or endpoints.z > 1:
                 satisfied = true;
 
-        # var positionX = (mesh.get_aabb() * global_transform).position.x
-        # var positionY = (mesh.get_aabb() * global_transform).position.y
+        print(get_contact_count());
 
-        # for i in range(8):
-        #     var endpoints = mesh.aabb.get_endpoint(i)
-        #     print(endpoints)
-
-        # print(positionX);
-
-        # if positionX < -1 or positionX > 1:
-        #     satisfied = true;
-        # elif positionY < -1 or positionY > 1:
-        #     satisfied = true;
+func _on_area_3d_body_entered(body: Node3D) -> void:
+    if !satisfied:
+        if self == body: return;
+        scale = oldscale;
+        satisfied = true;
