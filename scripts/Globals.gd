577c5dc7a848575efbefca2768f7ba3eb9b6e1b1
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-08-17 16:53:39 +0100

    animations, introduced bug that allows user to place after water risen

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -2,3 +2,9 @@ extends Node
 
 var time_elapsed = 0;
 var camera_mode = 0;
+var water_risen = false;
+var waterLevel = 0; # Goes from 0-1.
+
+func resetWater():
+    waterLevel = 0;
+    water_risen = false;
